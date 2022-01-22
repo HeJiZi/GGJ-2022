065b5437d99138e0cbd8bdad0b010e93d9a4185d
keeeepgo <keeeepgoing@163.com>
2022-01-22 14:00:00 +0800

    关卡选择

---
+++ Assets/Scripts/LevelSelectView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectView : MonoBehaviour
+{
+    public RectTransform root;
+    public List<string> levelNames;
+
+    public GameObject elementPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = levelNames.Count-1; i >= 0; i--)
+        {
+            var go = Instantiate(elementPrefab, root, true); 
+            var element = go.GetComponent<LevelSelectElement>();
+            element.Refresh(levelNames[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/OptionView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OptionView : MonoBehaviour
+{
+    public Button btnBack;
+
+    public string backScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnBack.onClick.AddListener(() =>
+        {
+            Debug.LogError(backScene);
+            SceneManager.LoadScene(backScene);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
