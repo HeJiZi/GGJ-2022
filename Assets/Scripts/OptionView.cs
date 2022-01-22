6416c58c2ede99a45045df3269469d2f4763156b
keeeepgo <keeeepgoing@163.com>
2022-01-22 14:08:28 +0800

    关卡信息界面

--- Assets/Scripts/OptionView.cs
+++ Assets/Scripts/OptionView.cs
@@ -7,15 +7,27 @@ using UnityEngine.UI;
 public class OptionView : MonoBehaviour
 {
     public Button btnBack;
-
+    public Button btnNext;
+    public Text textCurrentScene;
     public string backScene;
+
+    public string nextScene;
     // Start is called before the first frame update
     void Start()
     {
+        Scene scene = SceneManager.GetActiveScene ();
+        textCurrentScene.text = scene.name;
         btnBack.onClick.AddListener(() =>
         {
-            Debug.LogError(backScene);
+            Debug.Log("back "+backScene);
             SceneManager.LoadScene(backScene);
+        });
+
+        btnNext.gameObject.SetActive(!string.IsNullOrEmpty(nextScene)); 
+        btnNext.onClick.AddListener(() =>
+        {
+            Debug.Log("next "+nextScene);
+            SceneManager.LoadScene(nextScene);
         });
     }
 
